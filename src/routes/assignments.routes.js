7f8e93fd38acf1dfb8799316c3cdab627ce31f3b
Matias Gomez <62826588+matiasg99@users.noreply.github.com>
2025-11-03 22:30:49 -0300

    Revert "view"

--- src/routes/assignments.routes.js
+++ src/routes/assignments.routes.js
@@ -5,8 +5,8 @@ const ctrl = require("../controllers/assignments.controller");
 const auth = require("../middlewares/auth.js");
 const checkRole = require("../middlewares/roles.js");
 
-router.get("/", auth, checkRole("admin", "gestor", "trabajador"), ctrl.getAll);
-router.get("/activity/:activityId", auth, checkRole("admin", "gestor", "trabajador"), ctrl.getByActivity);
+router.get("/", auth, ctrl.getAll);
+router.get("/activity/:activityId", auth, ctrl.getByActivity);
 router.post("/", auth, checkRole("admin", "gestor"), ctrl.create);
 router.put("/:id", auth, checkRole("admin", "gestor"), ctrl.update);
 router.put("/:id/unassign", auth, checkRole("admin", "gestor"), ctrl.unassign);
